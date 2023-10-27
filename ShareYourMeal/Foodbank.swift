c994c5960a4ea7926bfbf2e0b947883bba07df68
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-10-27 15:50:42 +0100

    Creates a neededItems functions that strips out and sorts the needs from items.

--- ShareYourMeal/Foodbank.swift
+++ ShareYourMeal/Foodbank.swift
@@ -11,7 +11,7 @@ struct Foodbank: Codable, Identifiable {
     
     enum CodingKeys: String, CodingKey {
         //list the coding keys we care about, also where we tell it that the distance property is mapped to a different name on the JSON, if you don't specify a raw value it'll become that thing.
-        case name, slug, phone, email, address, distance = "distance_m"
+        case name, slug, phone, email, address, distance = "distance_m", items = "needs"
     }
     
     var id: String { slug }
@@ -22,11 +22,30 @@ struct Foodbank: Codable, Identifiable {
     var address: String
     //the below property doesn't exist on the JSON - codingkeys
     var distance: Int
+    //in the JSON the below is called needs, hence another Coding Key
+    var items: Items
     
     var distanceFormatted: String {
         let measurement = Measurement(value: Double(distance), unit: UnitLength.meters)
         //makes a string from the above formatted correctly. This automatically takes into account the users local settings for us.
         let measurementString = measurement.formatted(.measurement(width: .wide))
         return "\(measurementString) from you"
+    }
+    //takes the items object, pull its needs out, break it up by line breaks, remove duplicates and sort the result.
+    var neededItems: [String] {
+        //give me this thing as a array of strings
+        let baseList = items.needs.components(separatedBy: .newlines)
+        //set removes duplicates
+        return Set(baseList).sorted()
+    }
+}
+
+extension Foodbank {
+    struct Items: Codable, Identifiable {
+        var id: String
+        var needs: String
+        var excess: String?
+        
+        static let example = Items(id: "Example ID", needs: "Example Needed item", excess: "Example Excess item.")
     }
 }
