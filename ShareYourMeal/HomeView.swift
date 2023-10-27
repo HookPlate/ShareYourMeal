5bcc993d2607be4d693c9388c8b05c5636de347d
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-10-27 16:17:37 +0100

    Adds the new HomeView to the ContentView

---
+++ ShareYourMeal/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  ShareYourMeal
+//
+//  Created by robin tetley on 27/10/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
