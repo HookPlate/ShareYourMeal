f635af68df42405e987b29e492683fb21ebb0ad1
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-11-02 15:17:34 +0000

    A commit to satisfy git

---
+++ DropOffView.swift
@@ -0,0 +1,18 @@
+//
+//  DropOffView.swift
+//  ShareYourMeal
+//
+//  Created by robin tetley on 28/10/2023.
+//
+
+import SwiftUI
+
+struct DropOffView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropOffView()
+}
